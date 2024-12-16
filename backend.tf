4a5ae561b70037af70c580b16c68f6a9b3c2928c
Rutwik Dadgal <91602615+rutwikdadgal@users.noreply.github.com>
2024-12-16 23:17:51 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "pathkind-rg"
-    storage_account_name = "pathkindtfstatestgacc"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
